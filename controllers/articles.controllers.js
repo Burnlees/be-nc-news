1e29479747333e5c3d682c11afd14b59a491951a
burnlees <josh.burnlees@outlook.com>
2024-05-28 16:37:02 +0100

    creates the /api/articles/:article_id/comments endpoint

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,6 +1,7 @@
 const {
   selectArticleById,
   selectArticles,
+  selectCommentsByArticleId,
 } = require("../models/articles.models");
 
 exports.getArticles = (req, res, next) => {
@@ -17,4 +18,11 @@ exports.getArticleById = (req, res, next) => {
       res.status(200).send({ article: articleData });
     })
     .catch(next);
+};
+
+exports.getCommentsByArticleId = (req, res, next) => {
+  const { article_id } = req.params;
+  selectCommentsByArticleId(article_id).then((commentsData) => {
+    res.status(200).send({ comments: commentsData });
+  }).catch(next)
 };
