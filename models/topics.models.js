7f157207c3953783313f05c4dcbabccd934c54ed
burnlees <josh.burnlees@outlook.com>
2024-05-28 13:39:22 +0100

    removes console logs

--- models/topics.models.js
+++ models/topics.models.js
@@ -1,9 +1,7 @@
 const db = require("../db/connection");
 
 exports.selectTopics = () => {
-    console.log('inside model');
   return db.query(`SELECT * FROM topics`).then((res) => {
-    console.log(res.rows)
     return res.rows;
   });
 };
