a18a6c8c4b409e3b8c011c5546cb42dd1ea1fcde
burnlees <josh.burnlees@outlook.com>
2024-05-30 09:34:47 +0100

    fixing supabase issues

--- db/connection.js
+++ db/connection.js
@@ -9,9 +9,9 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-const config = {}
+const config = {};
 
-if (ENV === 'production') {
+if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
