afea095f1a85e0b74397ea516c92029b718fcaba
burnlees <josh.burnlees@outlook.com>
2024-06-17 09:46:37 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ const {
   deleteCommentById,
 } = require("./controllers/comments.controller");
 const apiRouter = require("./routes/api-router");
+const cors = require('cors');
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use("/api", apiRouter);
